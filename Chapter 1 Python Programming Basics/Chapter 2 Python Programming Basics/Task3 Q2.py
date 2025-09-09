f96895282c1963335a49ddd6621151b5c04a9a95
Maria Akmal <maria.nasralla50@gmail.com>
2025-09-09 15:49:21 +0300

    I addad tasks ch2

--- Chapter 1 Python Programming Basics/Chapter 2 Python Programming Basics/Task3 Q2.py
+++
@@ -1,9 +0,0 @@
-numbers = input("Enter numbers separated by space: ").split()
-numbers = [int(num) for num in numbers]
-number={}
-for num in numbers:
-    if num in number:
-        number[num]+=1
-    else:
-        number[num]=1
-print(number)
